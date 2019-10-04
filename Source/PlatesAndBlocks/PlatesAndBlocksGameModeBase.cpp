41d84f54010815c6e1243db56b289551af859ad7
Stanislav Brosalin <stanislav.brosalin@gmail.com>
2019-10-04 21:13:50 +0300

    PaB 11 - Release Ver. 1.0

--- Source/PlatesAndBlocks/PlatesAndBlocksGameModeBase.cpp
+++ Source/PlatesAndBlocks/PlatesAndBlocksGameModeBase.cpp
@@ -26,65 +26,21 @@ void APlatesAndBlocksGameModeBase::BeginPlay()
 	Super::BeginPlay();
 	
 	// X and Y axes according to UE4 project system coordinate
-	//Reset Board Actors Array Every Start or Repeat Game
-	if (!ensure(BoardActorsArray)) { return; }
-	for (int32 y = 0; y < Width; y++)
-	{
-		for (int32 x = 0; x < Height; x++)
-		{
-			BoardActorsArray[x][y] = nullptr;
-		}
-	}
 
 	InitializeColorArrays();
+
 	//At First Spawn Game Desk
 	//After that begin spawn plates and block 
 	//Plates Spawning at every even or zero column (start from 0)
 	//Blocks Spawning at odd columns and only at even or 0 rows (start from 0)
 	//A new plate at begin play calling GetRandomPlateColor Method to colorize the plate 
 	//After spawn a new plate or block copy pointer to BoardActorsArray
+ 	
+	SpawnDesk();
 
- 	ADesk* NewDesk = GetWorld()->SpawnActor<ADesk>(Desk_Blueprint, StartDeskLocation, FRotator::ZeroRotator);
+	SpawnColumnColors();
 
-	FVector NewLocation = StartColumnColorLocation;
-	for (int32 y = 0; y < Width; y++)
-	{
-		if (y % 2 == 0)
-		{
-			AColumnColor* NewColumnColor = GetWorld()->SpawnActor<AColumnColor>(ColumnColor_Blueprint, NewLocation, FRotator::ZeroRotator);
-
-			ColumnColorActorsArray[y] = NewColumnColor;
-		}
-		else
-		{
-			ColumnColorActorsArray[y] = nullptr;
-		}
-		NewLocation += FVector(0.f, 200.f, 0.f); // minus because x vector on the screen and in BoardActorsArray is the opposite directions
-	}
-	NewLocation = StartPlateLocation;
-	for (int32 y = 0; y < Width; y++)
-	{
-		for (int32 x = 0; x < Height; x++)
-		{
-			if (y % 2 == 0)
-			{
-				APlate* NewPlate = GetWorld()->SpawnActor<APlate>(Plate_Blueprint, NewLocation, FRotator::ZeroRotator);
-				NewPlate->SetPlacement(x, y);
-				BoardActorsArray[x][y] = NewPlate;
-			}
-			else if (x % 2 == 0 )
-			{
-				ABlock* NewBlock = GetWorld()->SpawnActor<ABlock>(Block_Blueprint, NewLocation, FRotator::ZeroRotator);
-				BoardActorsArray[x][y] = NewBlock;
-			}
-			else
-			{
-				BoardActorsArray[x][y] = nullptr;
-			}
-			NewLocation += FVector(-200.f, 0.f, 0.f); // minus because x vector on the screen and in BoardActorsArray is the opposite directions
-		}
-		NewLocation = StartPlateLocation + FVector(0.f, 200.f, 0.f)*(y + 1);
-	}
+	SpawnPlatesAndBlocks();
 }
 
 void APlatesAndBlocksGameModeBase::InitializeColorArrays()
@@ -101,29 +57,56 @@ void APlatesAndBlocksGameModeBase::InitializeColorArrays()
 	ColumnColorsArr.Add(EActorsColor::Red);
 }
 
-bool APlatesAndBlocksGameModeBase::CheckGameCompletion()
-{	
-	for (int y = 0; y < Width; y+=2) //Check only even or zero column
-	{	
-		EActorsColor ColumnColor = Cast<AColumnColor>(ColumnColorActorsArray[y])->GetColumnColor();
-		if (!ColumnColorActorsArray[y])
+void APlatesAndBlocksGameModeBase::SpawnDesk()
+{
+	ADesk* NewDesk = GetWorld()->SpawnActor<ADesk>(Desk_Blueprint, StartDeskLocation, FRotator::ZeroRotator);
+}
+
+void APlatesAndBlocksGameModeBase::SpawnColumnColors()
+{
+	FVector NewLocation = StartColumnColorLocation;
+	for (int32 y = 0; y < Width; y++)
+	{
+		if (y % 2 == 0) //spawn ColumnColors only at even or zero column
 		{
-			return false;
+			AColumnColor* NewColumnColor = GetWorld()->SpawnActor<AColumnColor>(ColumnColor_Blueprint, NewLocation, FRotator::ZeroRotator);
+
+			ColumnColorActorsArray[y] = NewColumnColor;
 		}
-		for (int x = 0; x < Height; x++)
-		{		
-			if (BoardActorsArray[x][y] == nullptr)
-			{
-				return false;
-			}
-			if (ColumnColor != Cast<APlate>(BoardActorsArray[x][y])->GetPlateColor())
-			{
-				return false;
-			}
+		else
+		{
+			ColumnColorActorsArray[y] = nullptr;
 		}
+		NewLocation += ShiftWidth; 
 	}
-	Cast<APlateController>(GetWorld()->GetFirstPlayerController())->SetControlledPlate(nullptr);
-	return true;	
+}
+
+void APlatesAndBlocksGameModeBase::SpawnPlatesAndBlocks()
+{
+	FVector NewLocation = StartPlateLocation;
+	for (int32 y = 0; y < Width; y++)
+	{
+		for (int32 x = 0; x < Height; x++)
+		{
+			if (y % 2 == 0)
+			{
+				APlate* NewPlate = GetWorld()->SpawnActor<APlate>(Plate_Blueprint, NewLocation, FRotator::ZeroRotator);
+				NewPlate->SetPlacement(x, y);
+				BoardActorsArray[x][y] = NewPlate;
+			}
+			else if (x % 2 == 0)
+			{
+				ABlock* NewBlock = GetWorld()->SpawnActor<ABlock>(Block_Blueprint, NewLocation, FRotator::ZeroRotator);
+				BoardActorsArray[x][y] = NewBlock;
+			}
+			else
+			{
+				BoardActorsArray[x][y] = nullptr;
+			}
+			NewLocation += ShiftHeight; 
+		}
+		NewLocation = StartPlateLocation + ShiftWidth*(y+1); //Start from the beginning and shift in width according column number
+	}
 }
 
 EActorsColor APlatesAndBlocksGameModeBase::GetRandomPlateColor()
@@ -139,26 +122,17 @@ EActorsColor APlatesAndBlocksGameModeBase::GetRandomColumnColor()
 EActorsColor APlatesAndBlocksGameModeBase::GetRandomColorfromArray(TArray<EActorsColor> &ColorsArray)
 {
 	int32 ArrColorsSize = ColorsArray.Num();
-	UE_LOG(LogTemp, Warning, TEXT("%i:"), ArrColorsSize);
 
 	int32 randNum = FMath::RandRange(0, ArrColorsSize - 1);
-	UE_LOG(LogTemp, Warning, TEXT("Random element %i:"), randNum);
-
+	
 	if (randNum >= ColorsArray.Num())
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Request unexisted element at PlateColorsArr %i:"), randNum);
 		return EActorsColor::Unknown;
 	}
 	EActorsColor RetColor = ColorsArray[randNum];
 	ColorsArray.RemoveAt(randNum);
 
 	return RetColor;
-}
-
-
-void APlatesAndBlocksGameModeBase::UpdateBoardActorsArray(int32 x, int32 y, AActor* Actor)
-{
-	BoardActorsArray[x][y] = Actor;
 }
 
 bool APlatesAndBlocksGameModeBase::IsPlaceFreeAndCorrect(int32 x, int32 y)
@@ -171,3 +145,33 @@ bool APlatesAndBlocksGameModeBase::IsPlaceFreeAndCorrect(int32 x, int32 y)
 	return false;
 }
 
+void APlatesAndBlocksGameModeBase::UpdateBoardActorsArray(int32 x, int32 y, AActor* Actor)
+{
+	BoardActorsArray[x][y] = Actor;
+}
+
+bool APlatesAndBlocksGameModeBase::CheckGameCompletion()
+{
+	for (int y = 0; y < Width; y += 2) //Check only even or zero column
+	{
+		
+		EActorsColor ColumnColor = Cast<AColumnColor>(ColumnColorActorsArray[y])->GetColumnColor();
+		if (!ColumnColorActorsArray[y])
+		{
+			return false;
+		}
+		for (int x = 0; x < Height; x++)
+		{
+			if (BoardActorsArray[x][y] == nullptr)
+			{
+				return false;
+			}
+			if (ColumnColor != Cast<APlate>(BoardActorsArray[x][y])->GetPlateColor())
+			{
+				return false;
+			}
+		}
+	}
+	Cast<APlateController>(GetWorld()->GetFirstPlayerController())->SetControlledPlate(nullptr);
+	return true;
+}
--- Source/PlatesAndBlocks/PlatesAndBlocksGameModeBase.h
+++ Source/PlatesAndBlocks/PlatesAndBlocksGameModeBase.h
@@ -60,6 +60,17 @@ private:
 	//Get Number of each color plates and create 2 color array (Column, Plate)
 	void InitializeColorArrays();
 
+	//Spawn Desk objects on the screen
+	void SpawnDesk();
+
+	//Spawn ColumnColor objects on the screen
+	//And fill ColumnColorActorsArray by column color pointers
+	void SpawnColumnColors();
+
+	//Spawn ColumnColor objects on the screen
+	//And fill BoardActorsArray by plate and blocks pointers
+	void SpawnPlatesAndBlocks();
+
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 QtyEachColorPlates = 5;
 
@@ -87,9 +98,18 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 Height = 5;
 
+	FVector ShiftWidth = FVector(0.f, 200.f, 0.f);
+
+	// minus because x vector on the screen and in BoardActorsArray is the opposite directions
+	FVector ShiftHeight = FVector(-200.f, 0.f, 0.f); 
+
+	//Array for access to Plate actors and get Color
 	AActor*** BoardActorsArray;
+	//Array for access to ColumnColor actors and get Color
 	AActor** ColumnColorActorsArray;
 
+	//Array for initial colorize plates
 	TArray<EActorsColor> PlateColorsArr;
+	//Array for initial colorize columnColors
 	TArray<EActorsColor> ColumnColorsArr;
 };
